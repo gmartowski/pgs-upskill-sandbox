41b614255ed5eb2231ca54dfd296187970099d2a
Grzegorz Martowski <kontakt@stabsoft.pl>
2021-12-20 11:40:42 +0100

    06 event bubbling capturing

--- vanilla-js/src/05-event-propagation/05-event-propagation.js
+++
@@ -1,8 +0,0 @@
-document.body.addEventListener('click', () => {
-    console.log('Klik w body');
-});
-
-document.querySelector('#bubble').addEventListener('click', () => {
-    console.log('Klik w button');
-});
-
--- vanilla-js/src/app.js
+++ vanilla-js/src/app.js
@@ -1,1 +1,17 @@
-import '05-event-propagation/05-event-propagation.js';
+document.body.addEventListener('click', (e) => {
+    console.log('Klik w body');
+});
+
+document.querySelector('#bubble').addEventListener('click', (e) => {
+    console.log(e.target, e.currentTarget);
+    console.log('Klik w button');
+});
+
+document.getElementById('test-ul').addEventListener('click', (e) => {
+    console.log(e.target.nodeName);
+    if (e.target.nodeName === 'LI') {
+        console.log('cclicked li:', e.target)
+    }
+})
+
+
