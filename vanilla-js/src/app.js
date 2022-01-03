adfc135eb4dc787e09747299670a261f5779edcd
gmartowski <kontakt@stabsoft.pl>
2022-01-03 16:22:06 +0100

    Closure training

--- vanilla-js/src/app.js
+++ vanilla-js/src/app.js
@@ -1,17 +1,52 @@
 var exec1 = 'exec';
 
 function Execution1() {
-    const exec2 = 'exec2';
+  const exec2 = 'exec2';
 
-    this.test1 = 1
-    this.test2 = 4;
+  this.test1 = 1
+  this.test2 = 4;
 
-    function Execution2() {
-        this.test3 = 'test3';
-        return this.test3 + 2;
-    }
+  function Execution2() {
+    this.test3 = 'test3';
+    return this.test3 + 2;
+  }
 
-    return this.test1 + exec1;
+  return this.test1 + exec1;
 }
+
 const exe = new Execution1();
 console.log(exe);
+
+const init = () => {
+  let counter = 0;
+
+  return {
+    add() {
+      return counter += 1;
+    }
+  }
+}
+
+
+const aaa = init();
+console.log(aaa.add());
+
+console.log(aaa.add());
+
+function couter (){
+  let counter = 0;
+
+  function add(){
+    return counter+=1
+  }
+
+  return add;
+}
+
+const t = couter();
+// console.error(t());
+// console.error(t());
+// console.error(t());
+
+
+
