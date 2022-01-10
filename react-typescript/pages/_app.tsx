04ea36825919073d2c25773208ad0671729ebc91
gmartowski <kontakt@stabsoft.pl>
2022-01-10 10:58:13 +0100

    React hooks

--- react-typescript/pages/_app.tsx
+++ react-typescript/pages/_app.tsx
@@ -1,8 +1,22 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {useEffect, useState} from "react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps) {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setCount(count + 1);
+  }, [])
+
+
+  return (
+    <div>
+      <button onClick={ () => setCount(count +1 )}>klik</button>
+      <div>Counter: {count}</div>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
